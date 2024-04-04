53ccbd52d93386d54d472631a8c024a8e4da4d90
Zhuojin Liu <lzj.cs@outlook.com>
2024-04-04 14:26:30 -0700

    Enable dynamically showing document inventory and beautify it

---
+++ Assets/Scripts/Document.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Document
+{
+    public string title;
+    public string content;
+
+    public Document(string title, string content)
+    {
+        this.title = title;
+        this.content = content;
+    }
+}
--- Assets/Scripts/DocumentInventoryController.cs
+++ Assets/Scripts/DocumentInventoryController.cs
@@ -11,6 +11,9 @@ public class DocumentInventoryController : MonoBehaviour
 
     private Button closeButton;
     private ListView inventoryList;
+    private List<Document> mockDocuments;
+    private Label title;
+    private Label content;
 
     private void OnEnable()
     {
@@ -25,8 +28,22 @@ public class DocumentInventoryController : MonoBehaviour
             var root = ui.rootVisualElement;
             closeButton = root.Q<Button>("closeButton");
             inventoryList = root.Q<ListView>("InventoryListView");
+            title = root.Q<Label>("Title");
+            content = root.Q<Label>("Content"); 
+
+            mockDocuments = new List<Document>()
+            {
+                new Document("Sample Document 1", "Content of the first document"),
+                new Document("Technical Report", "This report details the project findings..."),
+                new Document("User Manual", "Instructions on how to use the software effectively."),
+            };
 
             closeButton.RegisterCallback<ClickEvent>(ev => ui.rootVisualElement.style.display = DisplayStyle.None);
+            inventoryList.makeItem = MakeItem;
+            inventoryList.bindItem = BindItem;
+            inventoryList.itemsSource = mockDocuments;
+            inventoryList.RefreshItems();
+            inventoryList.Rebuild();
         }
     }
 
@@ -35,5 +52,26 @@ public class DocumentInventoryController : MonoBehaviour
         ui.enabled = true;
         InitializeUIElements();
         ui.rootVisualElement.style.display = DisplayStyle.Flex;
+    }
+
+    private VisualElement MakeItem()
+    {
+        var button = new Button();
+        button.AddToClassList("list-view-item");
+
+        return button;
+    }
+
+    private void BindItem(VisualElement element, int index)
+    {
+        var button = element as Button;
+        var document = mockDocuments[index];
+
+        button.text = document.title;
+        button.clicked += () =>
+        {
+            title.text = document.title;
+            content.text = document.content;
+        };
     }
 }
