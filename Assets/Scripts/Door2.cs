9f8e0935aae182ec6b693be2d47c7df5ecb0f819
Zhengyan Hu <91642985+Gloaming02@users.noreply.github.com>
2024-04-05 17:45:46 -0700

    add lobby and ticket tooth
    add lobby and ticket tooth

---
+++ Assets/Scripts/Door2.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door2 : MonoBehaviour
+{   
+    public GameObject popUpWindow;
+
+    void Start()
+    {   
+        // disable at the beginning
+        if (popUpWindow != null) {
+            popUpWindow.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        // check if door collider clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // get click position
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // check if the click hits the clickable area
+            if (GetComponent<Collider2D>().OverlapPoint(mousePos))
+            {
+                // click on the door, show pop up window
+                ShowPopUpWindow();
+            }
+        }
+    }
+
+    public void ShowPopUpWindow() {
+        
+        if (popUpWindow != null)
+        {
+            popUpWindow.SetActive(true);
+        }
+    }
+
+    // Button Yes
+    public void OnYesButtonClick() {
+        
+        //load scene
+        SceneManager.LoadScene("hall2");
+    }
+
+    public void OnNoButtonClick() {
+        
+        HidePopUpWindow();
+    }
+
+    public void HidePopUpWindow() {
+        
+        if (popUpWindow != null) {
+
+            popUpWindow.SetActive(false);
+        }
+    }
+}
